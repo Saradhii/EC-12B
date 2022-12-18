6b674ebecb5950525c3e45c24481b24e9d234bea
Saradhi <99667252+Saradhii@users.noreply.github.com>
2022-12-18 11:27:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const UserRoute = require("./routes/UserRoute");
 const NftRoute = require("./routes/NftRoute");
 
-
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
